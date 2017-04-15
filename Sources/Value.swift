3add9318abe6c85956dc345dc07824f5ceff2100
nixzhu <zhuhongxu@gmail.com>
2017-04-15 23:28:57 +0800

    hyper type url

--- Sources/Value.swift
+++ Sources/Value.swift
@@ -1,4 +1,6 @@
 
+import Foundation
+
 public enum Value {
     case null
     case bool(value: Bool, isRequired: Bool)
@@ -10,12 +12,20 @@ public enum Value {
     case string(value: String, isRequired: Bool)
     indirect case object(name: String, value: [String: Value], isRequired: Bool)
     indirect case array(name: String, value: [Value], isRequired: Bool)
+    // hyper type
+    case url(value: URL, isRequired: Bool)
 }
 
 extension Value {
 
     func updated(newName: String) -> Value {
         switch self {
+        case .string(value: let value, isRequired: let isRequired):
+            if let url = URL(string: value), let host = url.host, !host.isEmpty {
+                return .url(value: url, isRequired: isRequired)
+            } else {
+                return self
+            }
         case .object(name: _, value: let value, isRequired: let isRequired):
             var newValue: [String: Value] = [:]
             value.forEach { newValue[$0] = $1.updated(newName: $0) }
