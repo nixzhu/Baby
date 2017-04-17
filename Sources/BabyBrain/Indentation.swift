ed9f3f2b98685922f609f3baaee55113d9385aa5
nixzhu <zhuhongxu@gmail.com>
2017-04-17 13:45:17 +0800

    split Indentation

---
+++ Sources/BabyBrain/Indentation.swift
@@ -0,0 +1,21 @@
+
+public struct Indentation {
+    private let level: Int
+    private let unit: String
+
+    public init(level: Int, unit: String) {
+        self.level = level
+        self.unit = unit
+    }
+
+    static var `default`: Indentation {
+        return Indentation(level: 0, unit: "    ")
+    }
+
+    var value: String {
+        return String(repeating: unit, count: level)
+    }
+    var deeper: Indentation {
+        return Indentation(level: level + 1, unit: unit)
+    }
+}
